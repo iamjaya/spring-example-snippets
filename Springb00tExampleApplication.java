dc2e91c6e0967abce8ae8979c75a83a9ef636f3a
Jai@Coding <58899013+jai43@users.noreply.github.com>
2020-01-04 10:00:47 +0530

    SpringBootApplication

---
+++ Springb00tExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kitchen.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springb00tExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springb00tExampleApplication.class, args);
+	}
+
+}
