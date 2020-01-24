776f73dbb6f25c13d0253e9d1b6ac61a05c6e7cc
Jai@Coding <58899013+jai43@users.noreply.github.com>
2020-01-24 18:20:05 +0530

    Update StudentDAO.java

--- springBoot/jdbctemplate/StudentDAO.java
+++ springBoot/jdbctemplate/StudentDAO.java
@@ -1,8 +1,14 @@
-public StudentDAO implements StudentDAO_I
+public class StudentDAO implements StudentDAO_I
 {
 @Autowired
 JDBCTempalte jt;
 
+public int addStudent(Student s){
+
+String query="insert into student(studentid,studentname,branch,pincode) values (?,?,?,?)";
+int count = jt.update(query,new Object[]{s.studentId, s.studentName, s.branch, s.pincode});
+return count;
+}
 
 
 }
