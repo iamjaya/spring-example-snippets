3ebc98c99ecdc29dc7a9f0e75ed42a217b1c71cf
Jai@Coding <58899013+jai43@users.noreply.github.com>
2020-01-24 08:07:24 +0530

    Create StudentRowMapper.java

---
+++ springBoot/jdbctemplate/StudentRowMapper.java
@@ -0,0 +1,16 @@
+public StudentRowMapper implements RowMapper<Student>
+{
+@Override
+
+public Student mapRow(ResultSet rs, int rowNum) throws SQLException{
+
+Student s= new Student();
+s.setStudentId(rs.getInt("studentId"));
+s.setStudnetName(rs.getString("studentName"));
+s.setBranch(rs.getString("branch"));
+s.setPincode(rs.getInt("pincode"));
+return s;
+
+}
+
+}
